bfae4c997815f6878f1d712607584bda03d16dfd
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-10-24 23:06:54 -0200

    Adding model to invites
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- about_splash_page/home/models.py
+++ about_splash_page/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class InviteUser(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
--- about_splash_page/home/views.py
+++ about_splash_page/home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from home.models import InviteUser
 
 def index(request):
     if request.method == "POST":
         form = request.POST
+        first_name = form.get('first')
+        last_name = form.get('last')
         email = form.get('Email')
-        print(email)
-    return render(request, 'home/index.html')
+
+        invite_user = InviteUser.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email)
+        return render(request, 'home/thank.html')
+    else:
+        return render(request, 'home/index.html')
